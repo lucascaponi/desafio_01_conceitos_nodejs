f77e5e78250613bef95102f63ff3c46f52a6d8c7
LukasTsunami <prateadow@gmail.com>
2019-12-28 00:41:51 -0300

    Criando projetos

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,19 @@
 const globals = require("./globals");
 
-module.exports = function(app) {
-  app.get("/projects", (req, res) => {
+module.exports = function(server) {
+  server.get("/projects", (req, res) => {
+    res.json(globals.projects);
+  });
+
+  server.post("/projects", (req, res) => {
+    const { id, title, tasks } = req.body;
+
+    globals.projects.push({
+      id,
+      title,
+      tasks
+    });
+
     res.json(globals.projects);
   });
 };
